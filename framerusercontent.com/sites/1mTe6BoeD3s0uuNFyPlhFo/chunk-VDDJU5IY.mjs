5763c70f98b7fab6778b4bdc1c86e2c5d1a2ef7e
avagyanmher <78161847+avagyanmher@users.noreply.github.com>
2024-12-11 22:49:43 +0400

    Add files via upload

---
+++ framerusercontent.com/sites/1mTe6BoeD3s0uuNFyPlhFo/chunk-VDDJU5IY.mjs
@@ -0,0 +1,2 @@
+function t(e,a){return{description:"Digital Marketing Expert | Marketer, UI/UX Designer, Developer portfolio | Start your business growth ",favicon:"https://framerusercontent.com/assets/hQeMMS8498qshbhg3LDmzDGrEYk.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/yKF85HBtmvYzNDbHn9VzENWTNU.png",title:"Mher Avagyan"}}export{t as a};
+//# sourceMappingURL=chunk-VDDJU5IY.mjs.map
